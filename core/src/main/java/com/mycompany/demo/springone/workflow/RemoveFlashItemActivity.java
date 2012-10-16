9201b460788a3dce5e492054679f1bd1dee270df
Kelly Tisdell <ktisdell@broadleafcommerce.org>
2012-10-16 17:14:20 -0500

    Stubbing out activities

--- core/src/main/java/com/mycompany/demo/springone/workflow/RemoveFlashItemActivity.java
+++ core/src/main/java/com/mycompany/demo/springone/workflow/RemoveFlashItemActivity.java
@@ -1,9 +1,15 @@
 package com.mycompany.demo.springone.workflow;
 
+import javax.annotation.Resource;
+
+import org.broadleafcommerce.core.inventory.service.InventoryService;
 import org.broadleafcommerce.core.workflow.BaseActivity;
 import org.broadleafcommerce.core.workflow.ProcessContext;
 
 public class RemoveFlashItemActivity extends BaseActivity {
+	
+	@Resource(name="blInventoryService")
+	protected InventoryService inventoryService;
 
 	@Override
 	public ProcessContext execute(ProcessContext context) throws Exception {
